c7a73abc3f09c1d3fa4bdaa5620a3c7b5d57af9f
Andrés Patiño <andresfelipepatino5@gmail.com>
2021-05-21 12:01:04 -0500

    Fix mega menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,45 @@
 jQuery(document).ready(function ($) {
 
     // -- Mega menu
-    function inicializa (){
-        $(".DSMCA_marcas-motos .swiper-slide-image").each(function(){
-            let alt = $(this).attr("alt")
-            $(this).attr('data-marca' , alt) 
-        });
+	$(document).on('elementor/popup/show', () => {		
+		$(".DSMCA_marcas-motos .swiper-slide-image").each(function(){
+			let alt = $(this).attr("alt")
+			$(this).attr('data-marca' , alt) 
+		});
+		$(".DSMCA_marcas-motos .swiper-slide-image").mouseenter(function() {
+			let marca = $(this).attr("data-marca")
+			if( $('.DSMCA_motos.marca-' + marca).length > 0) {
+				$('.DSMCA_motos').each(function(){
+					if( $(this).is(':visible') ){
+						$(this).hide()
+					}
+				})
+				$('.DSMCA_motos.marca-' + marca).show();
+			}
+		});
+		$('.item-motos').mouseenter(function(){  
+			$('.DSMCA_template-mega-menu').each(function(){
+				if( $(this).is(':visible') && $(this).attr('data-item') != 'motos' ){
+					$(this).hide()
+				}
+				if( $(this).attr('data-item') == 'motos'  ){
+					$(this).show()
+				}
+			});
+			
+			// Fix slide marcas on load popup
+			let marcas_motos = jQuery('.DSMCA_marcas-motos .swiper-container');
+			let marcas_motos_instance = marcas_motos.data('swiper');
+			marcas_motos_instance.update();
+		});
+		
+		// Hide others mega menu
+		$('#side-menu .menu-item:not(.mega-menu)').mouseenter(function() {
+			$('.DSMCA_template-mega-menu').hide();
+		});		
+		
+	});	
 
-        $(".DSMCA_marcas-motos .swiper-slide-image").mouseenter(function() {
-            let marca = $(this).attr("data-marca")
-            if( $('.DSMCA_motos.marca-' + marca).length > 0) {
-                $('.DSMCA_motos').each(function(){
-                    if( $(this).is(':visible') ){
-                        $(this).hide()
-                    }
-                })
-                $('.DSMCA_motos.marca-' + marca).show();
-            }
-        });
-    }   
-
-    $('.item-motos').mouseenter(function(){       
-        $('.DSMCA_template-mega-menu').each(function(){
-            if( $(this).is(':visible') && $(this).attr('data-item') != 'motos' ){
-                $(this).hide()
-            }
-            if( $(this).attr('data-item') == 'motos'  ){
-                $(this).show()
-            }
-        })
-        setTimeout(inicializa, 500);
-    })
 
 
     // Slide product gallery
@@ -38,8 +47,8 @@ jQuery(document).ready(function ($) {
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
-		prevArrow: '<img class="slick-prev slick-arrow" src="/dismerca/wp-content/uploads/left.png">',
-		nextArrow: '<img class="slick-next slick-arrow" src="/dismerca/wp-content/uploads/right.png">',
+		prevArrow: '<img class="slick-prev slick-arrow" src="/wp-content/uploads/left.png">',
+		nextArrow: '<img class="slick-next slick-arrow" src="/wp-content/uploads/right.png">',
         fade: true,
         cssEase: 'linear',
         asNavFor: '.slider-nav',
